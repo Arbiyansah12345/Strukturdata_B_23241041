d10bc62aacc8111c7df61fd17b7806c8f230a606
ARBIYAN12 <148713185+Arbiyansah12345@users.noreply.github.com>
2024-04-29 11:54:09 +0800

    Add files via upload

---
+++ meet4&.py
@@ -0,0 +1,56 @@
+# Input dari user
+# arimatika
+
+# Belajar Inputan
+arbiyan = input("masukkan kata : ")
+print(" isi dari arbiyan :", arbiyan,"bertipe data :",type(arbiyan))
+
+#blajar aritmatika dasar
+x = 3
+y = 4
+
+# penjumlahan +
+hasil = x + y
+print ("x + y =",hasil )
+
+# pengurangan
+hasil = x - y
+print ("x - y =",hasil )
+
+# perkalian
+hasil = x * y
+print ("x * y =",hasil)
+
+# pembagian
+hasil = x / y
+print ("x / y =",hasil)
+
+# perpangkatan exponen **
+hsil = x ** y
+print ("x ^ y =",hasil)
+
+# modus %
+hasil = y % x
+print ("x mod y =",hasil)
+
+# flor division ( pembulatan kebawah) //
+hasil = x // y
+print ("x // y =",hasil)
+
+# aritmatika prioritas
+# (), exponen, perkalian/pembagian,penjumlahan/pengurangan
+x = 3
+y = 4
+z = 5
+
+hasil = ( x ** y * (z + x) / y - z % x // y)
+print(hasil)
+
+
+
+
+
+
+
+
+
