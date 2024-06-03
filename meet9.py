9047ac5e26b9b3bca7aff0c9b77c54b3e7e48b60
ARBIYAN12 <148713185+Arbiyansah12345@users.noreply.github.com>
2024-06-03 11:45:13 +0800

    Add files via upload
    3 juni 2024

---
+++ meet9.py
@@ -0,0 +1,22 @@
+# percabangan jika mereka
+
+name = input("what is your name : ")
+
+#if kondisi : aksi
+
+#if name == "arbiyan" : print (" ")
+#   print(" end of program")
+#   print ("end of peogram")
+#if identitas
+if name== "arbiyan":
+ #   print ("mahasiswa ganteng")
+ #   print ("ganteng banget")
+#print(" end of program")    
+    
+# 3. IF ELSE
+ if name == "arbiyan" :
+    print("mahasiswa ganteng")
+    print(f"terimakasih {name}")
+else :
+    print("anda tidak ganteng")
+print("end of program")
